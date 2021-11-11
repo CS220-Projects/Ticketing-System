43422add5e1255338836fa409967f547bc899937
Jack Rogerson <24610171+jrogerson@users.noreply.github.com>
2021-11-11 18:08:16 -0500

    Implement First Batch Of Models.

--- django/djangoproject/TicketingSystem/admin.py
+++ django/djangoproject/TicketingSystem/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 
-from .models import
+from .models import Franchise, RestaurantLocation, Item, Staff
+
+admin.site.register(Franchise)
+admin.site.register(RestaurantLocation)
+admin.site.register(Item)
+
+admin.site.register(Staff)
 
--- django/djangoproject/TicketingSystem/models.py
+++ django/djangoproject/TicketingSystem/models.py
@@ -1,3 +1,40 @@
 import uuid
 
 from django.db import models
+
+class Franchise(models.Model):
+    franchiseID = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    franchiseName = models.CharField(max_length = 500)
+    franchiseLocation = models.CharField(max_length = 500)
+
+
+class RestaurantLocation(models.Model):
+    class Meta:
+        unique_together = (('franchiseID','restaurantID'),)
+
+    franchiseID = models.ForeignKey("Franchise", null = False, on_delete = models.CASCADE, related_name = "Restaurant_Franchise")
+    restaurantID = models.UUIDField(default = uuid.uuid4, editable = False)
+    ManagerID = models.ForeignKey("Staff", null = True, on_delete = models.CASCADE, related_name = "Restaurant_Manager")
+    restaurantLocation = models.CharField(max_length = 500)
+    restaurantName = models.CharField(max_length = 500)
+
+
+class Item(models.Model):
+    itemID = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    itemName = models.CharField(max_length = 500)
+    itemPrice = models.DecimalField(max_digits = 20, decimal_places = 2)
+
+
+
+
+
+class Staff(models.Model):
+    staffID = models.UUIDField(default = uuid.uuid4, editable = False)
+    restaurantID = models.ForeignKey("RestaurantLocation", null=False, on_delete=models.CASCADE,related_name="Restaurant_Manager")
+    franchiseID = models.ForeignKey("Franchise", null = False, on_delete = models.CASCADE, related_name = "Restaurant_Manager")
+    staffName = models.CharField(max_length = 500)
+    staffStartDate = models.DateTimeField(auto_now=True)
+    staffTenure = models.CharField(max_length = 500)
+    staffGender = models.CharField(max_length = 500)
+    staffSalary = models.DecimalField(max_digits = 20, decimal_places = 2)
+
