f64799c87c0d2c7b31701a83554d32cf4df61216
Jack Rogerson <24610171+jrogerson@users.noreply.github.com>
2021-11-11 19:43:53 -0500

    Add String Representation for Models.

--- django/djangoproject/ticketing_system/models.py
+++ django/djangoproject/ticketing_system/models.py
@@ -7,6 +7,9 @@ class Franchise(models.Model):
     franchiseName = models.CharField(max_length = 500)
     franchiseLocation = models.CharField(max_length = 500)
 
+    def __str__(self):
+        return f"Franchise Name = {self.franchiseName} | Franchise Location = {self.franchiseLocation}"
+
 
 class RestaurantLocation(models.Model):
     class Meta:
@@ -18,11 +21,17 @@ class RestaurantLocation(models.Model):
     restaurantLocation = models.CharField(max_length = 500)
     restaurantName = models.CharField(max_length = 500)
 
+    def __str__(self):
+        return f"Restaurant Name = {self.restaurantName} | Restaurant Location = {self.restaurantLocation}"
+
 
 class Item(models.Model):
     itemID = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     itemName = models.CharField(max_length = 500)
     itemPrice = models.DecimalField(max_digits = 20, decimal_places = 2)
+
+    def __str__(self):
+        return f"Item Name = {self.itemName} | Item Price = {self.itemPrice}"
 
 
 
@@ -37,4 +46,7 @@ class Staff(models.Model):
     staffTenure = models.CharField(max_length = 500)
     staffGender = models.CharField(max_length = 500)
     staffSalary = models.DecimalField(max_digits = 20, decimal_places = 2)
+
+    def __str__(self):
+        return f"Staff Name = {self.staffName}"
 
