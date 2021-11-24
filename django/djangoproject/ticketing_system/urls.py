71477e4fa28e04338c870ae15f8a3b92914a477a
Jack Rogerson <24610171+jrogerson@users.noreply.github.com>
2021-11-24 12:18:09 -0500

    Add Test Form

--- django/djangoproject/ticketing_system/urls.py
+++ django/djangoproject/ticketing_system/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('test_form/', views.TestView, name="TestForm"),
     path('', views.index, name="index"),
 ]
--- django/djangoproject/ticketing_system/views.py
+++ django/djangoproject/ticketing_system/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+
+from .forms import TestForm
+
 
 #from .models import Status
 
 def index(request):
     context = {}
     return render(request, "index.html", context)
+
+
+
+
+
+def TestView(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = TestForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TestForm()
+
+    return render(request, 'TestForm.html', {'form': form})
