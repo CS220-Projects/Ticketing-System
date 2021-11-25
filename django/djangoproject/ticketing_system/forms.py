23d23f121ccd0d9c3180417370d1e01555e04245
Jack Rogerson <24610171+jrogerson@users.noreply.github.com>
2021-11-25 00:09:53 -0500

    Add CSS And Styling To Form

--- django/djangoproject/ticketing_system/forms.py
+++ django/djangoproject/ticketing_system/forms.py
@@ -1,3 +1,4 @@
+from django.forms import ModelForm, TextInput, Textarea, NumberInput, CheckboxInput, Select
 from django import forms
 from django.db import models
 
@@ -5,22 +6,60 @@ from .models import Franchise, Restaurant_Location, Item, Ticket, Customer_On_Ti
     Staff_On_Ticket, Ticket_Item, Staff, Customer
 
 
-class ticket(forms.ModelForm):
-    class Meta:
-        model = Ticket
-        fields = ("timeToFulfill", "specialRequests", "completedStatus", "tip")
-
 class customerOnTicket(forms.ModelForm):
+    """
+
+    """
     class Meta:
         model = Customer_On_Ticket
         fields = ("customerID",)
+        widgets = {
+            'customerID': Select(attrs={
+                'class': "form-select",
+                'style': 'margin: 20px;',
+            }),
+        }
+
 
 class staffOnTicket(forms.ModelForm):
+    """
+
+    """
     class Meta:
         model = Staff_On_Ticket
         fields = ("staffID",)
-        
-        
+        widgets = {
+            'staffID': Select(attrs={
+                'class': "form-select",
+                'style': 'margin: 20px;',
+            }),
+        }
 
-# class TestForm(forms.Form):
-#     subjectTest = forms.CharField(max_length=100)
+
+class ticket(ModelForm):
+    """
+
+    """
+    class Meta:
+        model = Ticket
+        fields = ("timeToFulfill", "specialRequests", "tip", "completedStatus")
+        widgets = {
+            'timeToFulfill': NumberInput(attrs={
+                'class': "form-control",
+                'style': 'margin: 20px',
+                'placeholder': '50'
+            }),
+            'specialRequests': Textarea(attrs={
+                'class': "form-control",
+                'style': 'margin: 20px',
+                'placeholder': 'Extra Sause Please'
+            }),
+            'tip': NumberInput(attrs={
+                'class': "form-control",
+                'style': 'margin: 20px;',
+                'placeholder': '20'
+            }),
+            'completedStatus': CheckboxInput(attrs={
+                'style': 'width: 20px; height: 20px; margin: 20px;',
+            }),
+        }
