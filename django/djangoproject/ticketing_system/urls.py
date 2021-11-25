c7122de6d4d2b0e1ebb095bfbefd7f587196591b
Jack Rogerson <24610171+jrogerson@users.noreply.github.com>
2021-11-25 00:16:04 -0500

    Implement New Ticket Functionality

--- django/djangoproject/ticketing_system/urls.py
+++ django/djangoproject/ticketing_system/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test_form/', views.TestView, name="TestForm"),
+    path('new-ticket/', views.NewTicketView, name="NewTicket"),
     path('success/', views.successView, name="SuccessView"),
     path('', views.indexView, name="indexView"),
 ]
--- django/djangoproject/ticketing_system/views.py
+++ django/djangoproject/ticketing_system/views.py
@@ -1,58 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-
 from .forms import ticket
 from .forms import customerOnTicket
 from .forms import staffOnTicket
 
-
 #from .models import Status
 
 
-
 def indexView(request):
+    """
+
+    """
     context = {}
     return render(request, "index.html", context)
 
+
 def successView(request):
+    """
+
+    """
     context = {}
     return render(request, "success.html", context)
 
 
+def NewTicketView(request):
+    """
 
-
-
-
-def TestView(request):
-    # if this is a POST request we need to process the form data
+    """
+    # if this is a POST request we need to process the form data.
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
+        #Create a form instance and populate it with data from the request:
         formTicket = ticket(request.POST)
         formCustomerOnTicket = customerOnTicket(request.POST)
         formStaffOnTicket = staffOnTicket(request.POST)
 
-        # check whether it's valid:
+        #Check for valid:
         if formTicket.is_valid() and formCustomerOnTicket.is_valid() and formStaffOnTicket.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            formTicket = formTicket.save()
+            #Process the data:
+            formTicket = formTicket.save()                                          #Save the ticket.
 
             formCustomerOnTicket = formCustomerOnTicket.save(commit=False)
-            formCustomerOnTicket.ticketID = formTicket
+            formCustomerOnTicket.ticketID = formTicket                              #Connect the customer to ticket.
             formCustomerOnTicket.save()
 
             formStaffOnTicket = formStaffOnTicket.save(commit=False)
-            formStaffOnTicket.ticketID = formTicket
+            formStaffOnTicket.ticketID = formTicket                                 #Connect the staff to ticket.
             formStaffOnTicket.save()
 
+            #Redirect to new URL:
             return HttpResponseRedirect('/ticketing_system/success/')
 
-    # if a GET (or any other method) we'll create a blank form
+    #If a GET (or other method), create a blank form.
     else:
         formTicket = ticket()
         formCustomerOnTicket = customerOnTicket()
         formStaffOnTicket = staffOnTicket()
 
-    return render(request, 'TestForm.html', {'formTicket': formTicket, "formCustomerOnTicket": formCustomerOnTicket, "formStaffOnTicket": formStaffOnTicket})
+    return render(request, 'NewTicket.html', {'formTicket': formTicket, "formCustomerOnTicket": formCustomerOnTicket, "formStaffOnTicket": formStaffOnTicket})
